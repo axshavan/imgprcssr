b7198c204ec9ee5b88ce6c32377ed4f0b5964093
Axshavan <axshavan@gmail.com>
2022-01-16 10:18:23 +0100

    new presets

--- run_presets.sh
+++ run_presets.sh
@@ -14,3 +14,5 @@ python3 run-cli.py $1 convertmatrix2,0.7,0.7,0.7,1,0.5,0 zerocolor,3 convertinvm
 python3 run-cli.py $1 convertmatrix2,1,0.5,0,0.2,1,0.5 multiplycolor,3,0.6 convertinvmatrix2,0.6,0.6,0,0.2,1,0.5
 python3 run-cli.py $1 convertmatrix2,0.945,1,0.376,0.235,0.148,0.849 scurve,3,75 convertinvmatrix2,0.945,1,0.376,0.235,0.148,0.849
 python3 run-cli.py $1 convertmatrix2,1,0.114,0.847,0.537,0.985,0.119 scurve,3 convertinvmatrix2,1,0.114,0.847,0.537,0.985,0.119
+python3 run-cli.py $1 convertmatrix2,0.912,0.147,0.867,0.068,0.422,0.099 scurve,3,100 convertinvmatrix2,0.912,0.147,0.867,0.068,0.422,0.099
+
